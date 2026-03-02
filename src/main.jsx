723ae1b81d61242bbc0067123aa10d0f9786708b
Oscar <oscar43k45@gmail.com>
2026-03-02 15:20:43 -0500

    Fix doble formulario

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// Importamos el componente raíz
 import App from "./App.jsx";
-// Importante: trae Tailwind a la app
 import "./index.css";
 
-// Punto de entrada de la app: renderiza <App /> dentro de #root
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
